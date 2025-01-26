e547a43b9cb07c638137e343d16d36337ae2c597
Zachary Roy <royzach1234@gmail.com>
2025-01-26 15:27:51 -0500

    Update Wave_gen.py

--- Wave_gen.py
+++ Wave_gen.py
@@ -1,3 +1,11 @@
-import board
-import digitalio
 import time
+import digitalio
+import board
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+while True:
+    led.value = True
+    time.sleep(0.1)
+    led.value = False
+    time.sleep(0.1)
